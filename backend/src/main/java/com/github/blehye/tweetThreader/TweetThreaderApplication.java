85c4d661737dc428ab9aa866d95b6a28a1a1649b
blehye <hhw0850@naver.com>
2025-03-24 01:07:44 +0900

    feat: spring boot 프로젝트 생성

---
+++ backend/src/main/java/com/github/blehye/tweetThreader/TweetThreaderApplication.java
@@ -0,0 +1,13 @@
+package com.github.blehye.tweetThreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetThreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetThreaderApplication.class, args);
+	}
+
+}
