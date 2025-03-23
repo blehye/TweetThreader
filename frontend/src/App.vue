e8ebf9edc6aa3ea713956ff9b30cb8628fda1c96
blehye <hhw0850@naver.com>
2025-03-24 02:23:07 +0900

    fix: App.vue 오류 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
 	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Welcome to Your Vue.js App" />
+	<TweetView />
 </template>
 
 <script>
-	import HelloWorld from './components/HelloWorld.vue';
+	import TweetView from './views/tweet/TweetView.vue';
 
 	export default {
 		name: 'App',
 		components: {
-			HelloWorld,
+			TweetView,
 		},
 	};
 </script>
